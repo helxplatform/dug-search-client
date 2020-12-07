b703679ae04d0e9a0311813b47691ad5e66e957d
Nathan Braswell <nbraswell@rti.org>
2020-12-07 17:58:03 -0500

    Working study variable output, processed in front end

--- src/components/search/result.js
+++ src/components/search/result.js
@@ -62,7 +62,9 @@ const StudyAccession = styled.div``
 export const Result = ({ result, query }) => {
     const { name, description, id, type } = result // other properties: type, search_terms, optional_terms
     const [knowledgeGraphs, setKnowledgeGraphs] = useState([])
+    const [variableResults, setVariableResults] = useState([])
     const { fetchKnowledgeGraphs } = useSearch()
+    const { fetchVariableResults } = useSearch()
 
     useEffect(() => {
         const getKgs = async () => {
@@ -70,6 +72,43 @@ export const Result = ({ result, query }) => {
             setKnowledgeGraphs(kgs)
         }
         getKgs()
+        const getVars = async () => {
+            const vars = await fetchVariableResults(id, query)
+            console.log("vars")
+            console.log(vars)
+            var groupedIds = vars.reduce((acc, obj) => {
+                let key = obj["study_id"]
+                if (!acc[key]) {
+                    acc[key] = []
+                }
+                acc[key].push(obj.id)
+                return acc
+            }, {})
+
+            var res = []
+            vars.reduce((thing, current) => {
+                const x = thing.find(item => item.study_id === current.study_id);
+                if (!x) {
+                    var cid = current.study_id
+                    var variableIds = groupedIds[cid]
+
+                    var studyObj = {
+                        study_id: current.study_id,
+                        study_name: current.study_name,
+                        variables: variableIds
+                    }
+
+                    res.push(studyObj)
+                    return thing.concat([current]);
+                } else {
+                    return thing;
+                }
+            }, []);
+            setVariableResults(res)
+        }
+        getVars()
+        console.log(knowledgeGraphs)
+        console.log(variableResults)
     }, [])
 
     return (
@@ -81,9 +120,29 @@ export const Result = ({ result, query }) => {
                 <strong>Description</strong>: { description } <br></br>
             </ResultParagraph>
             {
+                variableResults.length > 0 && variableResults.map(({ study_id, study_name, variables }) =>(
+                    <Collapser key={`${name} ${study_id}`} ariaId={'studies'} {...collapserStyles}
+                        title={
+                            <CollapserHeader>
+                                <StudyName>
+                                    <strong>Study</strong>:
+                                    <ExternalLink to={dbGapLink.study(study_id.replace(/^TOPMED\.STUDY:/, ''))} >{study_name}</ExternalLink>
+                                </StudyName>
+                                <StudyAccession>
+                                    <strong>Accession</strong>:
+                                    <ExternalLink to={dbGapLink.study(study_id.replace(/^TOPMED\.STUDY:/, ''))} >{study_id.replace(/^TOPMED\.STUDY:/, '')}</ExternalLink>
+                                </StudyAccession>
+                            </CollapserHeader>
+                        }
+                    >
+                        <VariablesList studyId={study_id.replace(/^TOPMED\.STUDY:/, '')} variables={variables} />
+                    </Collapser>
+                ))
+            }
+            {
                 knowledgeGraphs.length > 0 && (
-                    <Collapser key={ `${ name } kg` } ariaId={ `${ name } kg` } { ...collapserStyles } title={ <CollapserHeader>Knowledge Graph</CollapserHeader> }>
-                        <KnowledgeGraphs graphs={ knowledgeGraphs } />
+                    <Collapser key={`${name} kg`} ariaId={`${name} kg`} {...collapserStyles} title={<CollapserHeader>Knowledge Graph</CollapserHeader>}>
+                        <KnowledgeGraphs graphs={knowledgeGraphs} />
                     </Collapser>
                 )
             }
@@ -103,9 +162,16 @@ Result.propTypes = {
 }
 
 /*
+            {
+                knowledgeGraphs.length > 0 && (
+                    <Collapser key={ `${ name } kg` } ariaId={ `${ name } kg` } { ...collapserStyles } title={ <CollapserHeader>Knowledge Graph</CollapserHeader> }>
+                        <KnowledgeGraphs graphs={ knowledgeGraphs } />
+                    </Collapser>
+                )
+            }
 Section I had to take out because variables didn't work
             {
-                result.studies.map(({ study_id, study_name, variables }) => (
+                variableResults.studies.map(({ study_id, study_name, variables }) => (
                     <Collapser key={ `${ name } ${ study_id }` } ariaId={ 'studies' } { ...collapserStyles }
                         title={
                             <CollapserHeader>
--- src/hooks/use-search.js
+++ src/hooks/use-search.js
@@ -36,6 +36,7 @@ export const useSearch = () => {
 
     const fetchKnowledgeGraphs = async (concept_id, query) => {
         setIsLoadingKnowlegeGraphs(true)
+        console.log(query)
         const knowledgeGraphs = await axios.post(SEARCH_KG_URL, {
             index: 'kg_index',
             unique_id: concept_id,
@@ -54,20 +55,147 @@ export const useSearch = () => {
     }
     // This is a guess at what this function is supposed to be - JBC 29 NOV 2020
     const fetchVariableResults = async (concept_id, query) => {
+        console.log(query)
         setIsLoadingVariables(true)
         const variables = await axios.post(SEARCH_VAR_URL, {
             index: 'variables_index',
-            unique_id: concept_id,
+            concept: concept_id,
             query: query,
             size: 100,
         }).then(response => {
             console.log(response.data)
-            return response.data.result.hits.hits
+            var res = response.data.result.hits.hits
+            // var groupedIds = vars.reduce((acc,obj)=>{
+            //     let key = obj._source["study_id"]
+            //     if(!acc[key]){
+            //         acc[key] = []
+            //     }
+            //     acc[key].push(obj._source.id)
+            //     return acc
+            // }, {})
+
+            // var res = []
+            // vars.reduce((thing, current) => {
+            //     const x = thing.find(item => item._source.study_id === current._source.study_id);
+            //     if (!x) {
+            //         var cid = current._source.study_id
+            //         var variableIds = groupedIds[cid]
+
+            //         var studyObj = {
+            //             study_id: current._source.study_id,
+            //             study_name: current._source.study_name,
+            //             variables: variableIds
+            //         }
+
+            //         res.push(studyObj)
+            //         return thing.concat([current]);
+            //     } else {
+            //         return thing;
+            //     }
+            // }, []);
+
+            console.log(res)
+            return res
         })
         .catch(error => {
             console.error(error)
             return []
         })
+        // const variables = [
+        //     {
+        //         "_index": "variables_index",
+        //         "_type": "_doc",
+        //         "_id": "phv00087870.v6.p3",
+        //         "_score": 7.813047,
+        //         "_source": {
+        //             "id": "phv00087870.v6.p3",
+        //             "name": "",
+        //             "description": "",
+        //             "identifiers": [
+        //                 "PATO:0000068",
+        //                 "TOPMED.TAG:29",
+        //                 "ZFA:0000114",
+        //                 "XCO:0000197",
+        //                 "SO:0000905",
+        //                 "MONDO:0005009",
+        //                 "UBERON:0007100",
+        //                 "PATO:0001511",
+        //                 "MONDO:0005252",
+        //                 "foaf:status",
+        //                 "CHEBI:47867",
+        //                 "UBERON:0000948"
+        //             ],
+        //             "dataset_id": "pht001123.v7.p3",
+        //             "dataset_name": "",
+        //             "dataset_description": "",
+        //             "study_id": "phs000209.v13.p3",
+        //             "study_name": "Multi-Ethnic Study of Atherosclerosis (MESA)",
+        //             "study_description": ""
+        //         }
+        //     },
+        //     {
+        //         "_index": "variables_index",
+        //         "_type": "_doc",
+        //         "_id": "phv00087871.v6.p3",
+        //         "_score": 7.813047,
+        //         "_source": {
+        //             "id": "phv00087871.v6.p3",
+        //             "name": "",
+        //             "description": "",
+        //             "identifiers": [
+        //                 "PATO:0000068",
+        //                 "TOPMED.TAG:29",
+        //                 "ZFA:0000114",
+        //                 "XCO:0000197",
+        //                 "SO:0000905",
+        //                 "MONDO:0005009",
+        //                 "UBERON:0007100",
+        //                 "PATO:0001511",
+        //                 "MONDO:0005252",
+        //                 "foaf:status",
+        //                 "CHEBI:47867",
+        //                 "UBERON:0000948"
+        //             ],
+        //             "dataset_id": "pht001123.v7.p3",
+        //             "dataset_name": "",
+        //             "dataset_description": "",
+        //             "study_id": "phs000209.v13.p3",
+        //             "study_name": "Multi-Ethnic Study of Atherosclerosis (MESA)",
+        //             "study_description": ""
+        //         }
+        //     },
+        //     {
+        //         "_index": "variables_index",
+        //         "_type": "_doc",
+        //         "_id": "phv00087872.v6.p3",
+        //         "_score": 7.813047,
+        //         "_source": {
+        //             "id": "phv00087872.v6.p3",
+        //             "name": "",
+        //             "description": "",
+        //             "identifiers": [
+        //                 "PATO:0000068",
+        //                 "TOPMED.TAG:29",
+        //                 "ZFA:0000114",
+        //                 "XCO:0000197",
+        //                 "SO:0000905",
+        //                 "MONDO:0005009",
+        //                 "UBERON:0007100",
+        //                 "PATO:0001511",
+        //                 "MONDO:0005252",
+        //                 "foaf:status",
+        //                 "CHEBI:47867",
+        //                 "UBERON:0000948"
+        //             ],
+        //             "dataset_id": "pht001123.v7.p3",
+        //             "dataset_name": "",
+        //             "dataset_description": "",
+        //             "study_id": "phs000209.v13.p3",
+        //             "study_name": "Multi-Ethnic Study of Atherosclerosis (MESA)",
+        //             "study_description": ""
+        //         }
+        //     }
+        // ]
         setIsLoadingVariables(false)
         return variables.map(variables => variables._source)
     }
