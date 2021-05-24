ac0f92a48df1382d6684b9cd7b02373832a75823
Carl Schreep <schreepc@renci.org>
2021-05-24 10:47:58 -0400

    Import path for app test

--- src/app.test.js
+++ src/app.test.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { render } from '@testing-library/react';
-import App from './App';
+import App from './app';
 
 test('renders header', () => {
   const { getByText } = render(<App />);
